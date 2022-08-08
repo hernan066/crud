c58f2cd6b150fa2ff7de3e2e986c21e0de2f1d6f
hernan0066 <hernan0066@gmail.com>
2022-08-08 17:54:25 -0300

    Initial commit

---
+++ src/controllers/mainController.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+  index: (req, res) => {
+    const lastView = products.filter(
+      (product) => product.category === "visited"
+    );
+    const inSale = products.filter((product) => product.category === "in-sale");
+
+    return res.render("index", {
+		lastView, 
+		inSale,
+		toThousand
+	});
+  },
+  search: (req, res) => {
+    // Do the magic
+  },
+};
+
+module.exports = controller;
